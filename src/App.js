2b1013771171fb054143798a5dad66a84b0f6d32
Daniel <danielsollenni1996@gmail.com>
2022-12-06 23:37:07 -0300

    state and showing basic list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
-  "Do Homework";
-  return <div className={styles.App}></div>;
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    // const newTodoList = [...todoList, newTask];
+    setTodoList([...todoList, newTask]);
+  };
+
+  return (
+    <div className={styles.App}>
+      <div className="addTask">
+        <input type="text" onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task, key) => {
+          return <h1 key={key}>{task}</h1>;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
